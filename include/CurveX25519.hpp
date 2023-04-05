399ba55bc24672305991571d4843b5fa2cae94e1
Maxime Rambosson <maxime.rambosson@gmail.com>
2023-04-06 00:16:09 +0200

    Use 256 bits as return

--- include/CurveX25519.hpp
+++ include/CurveX25519.hpp
@@ -19,11 +19,11 @@ boost::multiprecision::uint256_t decode_u_coordinate(std::array<unsigned char, 3
 
 boost::multiprecision::uint256_t decode_scalar(std::array<unsigned char, 32> k);
 
-std::array<unsigned char, 32> encode_u_coordinate(boost::multiprecision::uint512_t u);
+std::array<unsigned char, 32> encode_u_coordinate(boost::multiprecision::uint256_t u);
 
 std::pair<boost::multiprecision::uint256_t, boost::multiprecision::uint256_t> cswap(unsigned char swap, boost::multiprecision::uint256_t x_2, boost::multiprecision::uint256_t x_3);
 
-boost::multiprecision::uint512_t scalar_multiplication(const boost::multiprecision::uint256_t& u, const boost::multiprecision::uint256_t& k);
+boost::multiprecision::uint256_t scalar_multiplication(const boost::multiprecision::uint256_t& u, const boost::multiprecision::uint256_t& k);
 
 std::array<unsigned char, 32> X25519(const std::array<unsigned char, 32>& u, const std::array<unsigned char, 32>& k);
 
