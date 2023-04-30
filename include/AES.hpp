bcb4941da6f615ed0c80ef65b4ff4162617e5aec
Maxime Rambosson <maxime.rambosson@gmail.com>
2023-05-01 01:42:14 +0200

    AES implementation

--- include/AES.hpp
+++ include/AES.hpp
@@ -2,5 +2,14 @@
 #define CSMSG_AES_HPP
 
 #include <array>
+#include <vector>
+
+std::array<uint8_t, 16> AES_block_encryption(const std::array<uint8_t, 16>& plaintext, const std::array<uint8_t, 32>& key);
+
+std::array<uint8_t, 16> AES_block_decryption(const std::array<uint8_t, 16>& cyphertext, const std::array<uint8_t, 32>& key);
+
+std::vector<uint8_t> CBC_AES_encryption(const std::vector<uint8_t>& msg, const std::array<uint8_t, 32>& key, std::array<uint8_t, 16> IV);
+
+std::vector<uint8_t> CBC_AES_decryption(const std::vector<uint8_t>& cyphertext, const std::array<uint8_t, 32>& key);
 
 #endif CSMSG_AES_HPP
