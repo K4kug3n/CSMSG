3d79cf93c5b536b0bc529d2c242cb6545195630d
Maxime Rambosson <maxime.rambosson@etu.unige.ch>
2023-05-03 00:35:37 +0200

    Implement KDF_RK & KDF_CK for ratchet

--- include/KDF.hpp
+++ include/KDF.hpp
@@ -6,6 +6,12 @@
 
 std::array<uint8_t, 32> KDF(std::vector<uint8_t> KM);
 
+std::vector<uint8_t> HKDF(const std::vector<uint8_t>& salt, const std::vector<uint8_t>& IKM, const std::vector<uint8_t>& info, size_t L);
+
 std::array<uint8_t, 64> HMAC_512(std::vector<uint8_t> key, const std::vector<uint8_t>& msg);
+
+std::pair<std::array<uint8_t, 32>, std::array<uint8_t, 32>> KDF_RK(const std::array<uint8_t, 32>& rk, std::array<uint8_t, 32> dh_out);
+
+std::pair<std::array<uint8_t, 32>, std::array<uint8_t, 32>> KDF_CK(const std::array<uint8_t, 32>& ck);
 
 #endif
