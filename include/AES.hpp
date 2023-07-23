98997296a5f7456c9fe0a2029def919034419eb5
Maxime Rambosson <maxime.rambosson@gmail.com>
2023-07-23 19:33:45 +0200

    Missing include

--- include/AES.hpp
+++ include/AES.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <vector>
+#include <cstdint>
 
 std::array<uint8_t, 16> AES_block_encryption(const std::array<uint8_t, 16>& plaintext, const std::array<uint8_t, 32>& key);
 
